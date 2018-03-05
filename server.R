0db28dda93c0507300c880b73a4bcd863dffffb4
A <gitemail@gitemail.com>
2018-03-05 15:13:29 +0100

    cleaning up

--- server.R
+++ server.R
@@ -3,26 +3,21 @@ library(plotly)
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
-  
     output$plot <- renderPlotly({
-      output$numberofdaysindataset <- reactive({TRUE})
+      #output$numberofdaysindataset <- reactive({TRUE})
       #days <- input$days
-
-      #rn <- which(csv_bitstamp$Date ==input$date)
-      #print(rn)
       
       selectedDay <- nrow(csv_bitstamp) - which(csv_bitstamp$Date ==input$date)
       dayrange <- selectedDay:nrow(csv_bitstamp)
+      
+      #get btc price on selected day
       btcPriceOnSelDay <- round(csv_bitstamp[which(csv_bitstamp$Date ==input$date),]$Close, digits = 2)
-      #print(btcPriceOnSelDay)
       
       buydate <- format(input$date, "%a %b %d %Y")
       
+      #find maximum closing price in datasite
       maxBtcClose <- max(csv_bitstamp$Close)
       dateMaxBtcClose <- format(csv_bitstamp[csv_bitstamp$Close == maxBtcClose,]$Date, "%a %b %d %Y")
-      
-      #print(paste(dateMaxBtcClose, " highest"))
-      #input$date
       
       #get BTC price on date input
       srow <- which(csv_bitstamp$Date == input$date)
@@ -40,15 +35,12 @@ shinyServer(function(input, output) {
       output$profit <- renderText({paste0("$", formatC(as.numeric(profit), format="f", digits=2, big.mark = ","))})
       output$factor <- renderText({paste(round(profit/input$buyamountusd, digits=2), "x")})
       
-      #output$priceonday <- renderText({paste("On ", buydate, " the price of 1 BTC equalled $", print(btcPriceOnSelDay))})
-      #output$profit <- renderText({paste("Buying in on ", buydate, " and spending $", input$buyamountusd, "until ", dateMaxBtcClose, "and then selling you would have made a profit of $", profit)})
-  
       #plot
       p <- plot_ly(data = csv_bitstamp, x = csv_bitstamp$Date[dayrange], y = csv_bitstamp$Close[dayrange], type = "scatter", mode = "lines", showlegend = TRUE, name = "BTC Closing Price") %>%
           add_trace(y = ~High[dayrange], type="scatter", mode="lines", fill = "tonexty", fillcolor = "rgba(0,100,80,0.2)", line = list(color="transparent"), showlegend = TRUE, name="Day High") %>%
           add_trace(y = ~Low[dayrange], type="scatter", mode="lines", fill = "tonexty", fillcolor = "rgba(0,100,80,0.2)", line = list(color="transparent"), showlegend = TRUE, name="Day Low")
           
-          #toggle for showing volume in plot
+          #toggle for showing volume in plot, not used anymore
           #if(input$showvolume){
           #  p <- p %>% add_trace(x = csv_bitstamp$Date[dayrange], y = csv_bitstamp$Volume..BTC.[dayrange]/10, type="bar", name = "Volume BTC x 10")
           #}
--- ui.R
+++ ui.R
@@ -8,46 +8,33 @@
 #
 
 library(shiny)
-options(shiny.trace=TRUE) 
+#options(shiny.trace=TRUE) 
 #options(shiny.reactlog=TRUE) 
-# Define UI for application that draws a histogram
-
-##Slide one
-
-##Slide two
 
 shinyUI(fluidPage(
-
-  # Application title
   titlePanel("Bitcoin Regret Calculator"),
-    h1("March 4th 2018"),
-    p("The "),
-  # Sidebar with a slider input for number of bins 
+    h3("March 5th 2018"),
+    p("The below application takes one year worth of bitcoin pricing data and allows the user to select a date from within that range and calculates 
+      the profit made if the user would have spend an x amount of US dollar buying bitcoin (input by user using a slider) and then selling on bitcoin's 
+      highest closing price (dec 16th 2017). Or if a date is selected that falls after that point how much the user would profit if the bitcoin price ever
+      reached that height again in the future."),
   sidebarLayout(
-    
     sidebarPanel(
-        #daysindataset <- as.numeric(uiOutput("numberofdaysindataset")),
-        #daysindataset <- output.daysindataset,
-        
-        #sliderInput("days","Number of days from today", min = 1, max = numberofdaysindataset, value = 14),
+        #show volume checkbox, not useful anymore
         #checkboxInput("showvolume", "Show Volume", FALSE),
+      
         dateInput("date", "Enter the date on which you first entertained the thought of buying some bitcoin...but didn't.", value= min(csv_bitstamp$Date), min = min(csv_bitstamp$Date), max = max(csv_bitstamp$Date), startview = "year"),
-        #p(textOutput("priceonday")),
         sliderInput("buyamountusd", "How much money could you realistically have put into bitcoin at that time?", min = 100, max = 10000, value = 100, pre="$"),
-        #p(textOutput("profit"))
-        #actionButton("calc", "Calculate profit", icon = icon("bitcoin")),
+
       fluidRow(column(width=12, tags$span("The price of 1 Bitcoin on", tags$strong(textOutput("buydate")), "was", tags$strong(textOutput("btcpriceonselday")), "USD"))),
       fluidRow(column(width=12, tags$span("Buying", tags$strong(textOutput("buyamount")), " USD worth of BTC and holding it until ", tags$strong(textOutput("datemaxbtcclose")), " and then selling would have made you a profit of ", tags$strong(textOutput("profit")), "Your investment would have increased by a factor of ", tags$strong(textOutput("factor")))))
     ),
         
-    
-    # Show a plot of the generated distribution
+    # Show a plot and some info on the dataset used
     mainPanel(
       fluidRow(column(width=12, tags$div("Bitcoin price data on ", a(href="https://www.bitstamp.net/","Bitstamp."), " Data from ", a(href="https://www.quandl.com/collections/markets/bitcoin-data", "Quandl.com")))),
-      fluidRow(column(width=12, tags$div("Showing bitcoin closing price from",  format(min(csv_bitstamp$Date), "%a %b %d %Y"), " to ", format(max(csv_bitstamp$Date), "%a %b %d %Y")))),
+      fluidRow(column(width=12, tags$div("Contains data on bitcoin price from",  format(min(csv_bitstamp$Date), "%a %b %d %Y"), " to ", format(max(csv_bitstamp$Date), "%a %b %d %Y")))),
       plotlyOutput("plot")
-      #fluidRow(column(width=12, tags$button(textOutput("priceonday")))),
-      #fluidRow(column(width=12, tags$h5(textOutput("profit"))))
       )
     )
   )
